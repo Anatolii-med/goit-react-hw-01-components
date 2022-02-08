cf079f2ccc44ed5b0ea2188f3a5a68f4180220b1
Anatolii <Anatolii-med@users.noreply.github.com>
2022-02-08 21:47:29 +0200

    1 render

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Profile from "./Components/Profile/profile";
+import user from "./Data/user.json";
+
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  );
 }
 
 export default App;
